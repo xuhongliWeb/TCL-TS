ded891e47bb83e9781ac1c2e2591bde4e86ffea7
xuhongliWeb <564507650@qq.com>
2023-12-22 16:45:11 +0800

    feat: includes

--- type-challenges/easy-unshift/temaplte.ts
+++ type-challenges/easy-unshift/temaplte.ts
@@ -5,3 +5,21 @@ type Unshift<T extends Array<unknown>,K> = [K,...T]
 function Unshift (T,K) {
     return [K,...T]
 }
+
+// type Itype = {
+//     age: number; //
+//     type: string; //
+// }
+
+// type IName = {
+//     name:string; //
+//     type:number; //
+// }
+
+// type Iall = Itype & IName; //
+
+// const obj:Iall = {
+//     age:1,
+//     type: '',
+//     name: ''
+// }
